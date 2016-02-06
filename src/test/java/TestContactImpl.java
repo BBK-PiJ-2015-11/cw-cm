126613fb61536937d26a7897a513fbcc04d4feb5
Matt Bostock <matt@mattbostock.com>
2016-02-06 18:40:24 +0000

    Add failing test for ContactImpl.getId()
    Add a failing test for the `getId()` method on the implementation of the
    Contact interface, `ContactImpl`. I'll add the code to satisfy this test
    in the next commit.
    
    Also define Junit as a dependency in `build.gradle` and use Maven
    Central as my library repository.

---
+++ src/test/java/TestContactImpl.java
@@ -0,0 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestContactImpl {
+  private Contact contact;
+  private int id;
+  private String name;
+  private String notes;
+
+  @Before
+  public void setUp() {
+    id = 9;
+    name = "Foo Bar";
+    notes = "Baz";
+    contact = new ContactImpl(id, name, notes);
+  }
+
+  @Test
+  public void testId() {
+    assertEquals(id, contact.getId());
+  }
+}
